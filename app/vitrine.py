f060b63457ab03a81c71d478c233bc71ed4f67df
theo7791l <c42439980@gmail.com>
2026-03-20 22:25:14 +0100

    fix: Corriger les noms de variables passées au template pour afficher les vraies statistiques de la DB

--- app/vitrine.py
+++ app/vitrine.py
@@ -18,7 +18,7 @@ def index():
         total_accounts = 0
         total_chars = 0
     return render_template('vitrine/index.html', articles=articles,
-                           total_accounts=total_accounts, total_chars=total_chars)
+                           total_total_users=total_accounts, total_characters=total_chars)
 
 
 @vitrine_bp.route('/about')
